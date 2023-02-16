e6135344cfb8627b1e49dc14b98167f81b6cadeb
pviprushi <124043873+pviprushi@users.noreply.github.com>
2023-02-16 14:33:34 +0530

    Delete rv16_instr_dmux.sv

--- rv16_instr_dmux.sv
+++
@@ -1,9 +0,0 @@
-module instr_dmux(input [15:0]rv16_dminstr_in, output [3:0]opcode_out, output[3:0]rs1_addr_out,
-                  output [3:0]rs2_addr_out, output [3:0]rd_addr_out);
-
-  assign opcode_out = rv16_dminstr_in[3:0];
-  assign rs1_addr_out	= rv16_dminstr_in[7:4];
-  assign rs2_addr_out	= rv16_dminstr_in[11:8];
-  assign rd_addr_out		= rv16_dminstr_in[15:12];
-
-endmodule
